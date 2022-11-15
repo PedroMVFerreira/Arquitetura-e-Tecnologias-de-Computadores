79aa73af6b00ae45ff45b20390771dd6bdaf9906
Pedro Ferreira <pmvferreira02@gmail.com>
2022-11-15 00:47:13 +0000

    desafio 6 v0.3

--- Praticas/Desafio6/desafio6.c
+++ Praticas/Desafio6/desafio6.c
@@ -1,63 +1,75 @@
 /*
 Autores: Pedro Vale Ferreira (a95699) | Davide Oliveira Peixoto (a93949)
-Data: 9 de novembro de 2022 | 17.13 
-Versão: 0.1
-Desafio 6
+Data: 09 de novembro de 2022 
+VersÃ£o: 0.3
+Desafio 2
 Turno: PL2
 Docente: Rui Machado 
 __________________________________________________________________________________________________________________
-Implementaão da maquina de estados referente ao exercicio e comunicação UART
-!!!Carece de teste 
+ImplementaÃ£o da maquina de estados referente ao exercicio e junção com comunicação UART
 */
-/*Definição dos estados*/
+/*DefiniÃ§Ã£o dos estados*/
 #define DISPLAY 0
 #define INC 1
 #define DEC 2
-/*Fim da definição dos estados*/
+/*Fim da definiÃ§Ã£o dos estados*/
+
+/*Variaveis globais*/
+signed char index;
+unsigned char letter;
 
 /*Prototipos*/
 void generalConfigs();
-void timer1Configs();
+void timerConfigsForUART0();
 void portaSerieConfigs();
-unsigned char recebeUART() ;
-/*Fim de Prototipos*/
+void receiveUART();
+void sendUART(signed char x);
+/*Fim de prototipos*/
 
 #include <c8051f380.h>
 void main(void)
 {
-
-    generalConfigs();/*Configurações gerais do micro controlador*/
-    timer1Configs();/*Configurações do timer 1*/
-    portaSerieConfigs();/*Configurações da porta serie*/
-
-	/*Declaração de variaveis*/
+	generalConfigs();
+	timerConfigsForUART0();
+	portaSerieConfigs();
+	/*Declaração e inicialização de variaveis*/
 	__code unsigned char digitsOnP2[] = {0xC0/*0*/, 0xF9/*1*/, 0xA4/*2*/, 0xB0/*3*/, 0x99/*4*/, 0x92/*5*/, 0x82/*6*/, 
 	0xF8/*7*/, 0x80/*8*/, 0x98/*9*/, 0x88/*A*/, 0x83/*b*/, 0xC6/*C*/, 0xA1/*d*/, 0x86/*E*/, 0x8E/*F*/};
-	unsigned char index = 0;
+	index = 0;
 	unsigned char state = DISPLAY;
-    unsigned char letter;
-	/*Fim da declaração das variavies*/
+	/*Fim da declaraÃ§Ã£o das variavies*/
 	
 	while(1){
-        letter = recebeUART(); /*Receção de 'i' ou 'I' para incremento ou receção de 'd' ou 'D' para decremento*/
+		if(RI0 = 1)
+			receiveUART();
 		switch(state){
 			case DISPLAY:
-				P2 = digitsOnP2[index & 0x0F];
-				if(P0_6 == 0 || letter == 'i' || letter == 'I'){
+				P2 = digitsOnP2[index];
+				if(P0_6 == 0){
 					while(P0_6 == 0);
 					state = INC;
 				}
-				if(P0_7 == 0 || letter == 'd' || letter == 'D'){
+				if(letter == 'i' || letter == 'I')
+					state = INC;
+				if(P0_7 == 0){
 					while(P0_7 == 0);
 					state = DEC;
 				}
+				if(letter == 'd' || letter == 'D')
+					state = DEC;
 			break;
 			case INC:
 				index++;
+				if(index > 15)/*Utilizou se o if e não a mascara por causa do envio do valor de indice*/
+					index = 0;
+				sendUART(index);
 				state = DISPLAY;
 			break;
 			case DEC:
 				index--;
+					if(index < 0)/*Utilizou se o if e não a mascara por causa do envio do valor de indice*/
+						index = 15;
+				sendUART(index);
 				state = DISPLAY;
 			break;
 		}/*Fim do switch case*/
@@ -66,37 +78,43 @@ void main(void)
 
 void generalConfigs()
 {
-    /*Faz disahble do watchdog*/
+	/*Faz disahble do watchdog*/
 	PCA0MD = 0x00;
 	/*Ativa o crossbar*/
 	XBR1 = 0x40;
-    /*Ativa UART0 no crossbar*/
-    XBR0 |= 0x01;  
+	/*Ativa UART0 no crossbar*/
+	XBR0 |= 0x01;
 }
 
-void timer1Configs()
+void timerConfigsForUART0()
 {
-    /*Iremos configurar o timer 1 para fornecer um baud rate (igual ao bite rate no nosso mircro)
-    de 115200 - segundo a tabela 22.1 do datasheet*/
-    CKCON |= 0X08; /*Equivalente a ter T1M = 1*/
-    TH1 = 0x98; /*Valor de reload para 115200 e 9600 (devido a este fator se comentarmos a linha
-    de cima obtemos um baud rate de 9600 bps em vez de 115200 bps)*/
-    TMOD |= 0x20; /*Timer 1 no modo 2 (8bit com auto reload - obrigatorio)*/
-    TR1 = 1; /*Ativação do timer 1*/
+	/*Iremos configurar o timer 1 responsavel pela alimentação da UART 0 para forncere um baud rate (igual ao bit 
+	rate no nosso micro) de 115200*/
+	CKCON |= 0x08; /*Equivalente a ter T1M  = 1*/
+	TH1 = 0x98; /*Valor de reload para 115200 (o valor de reload para 9600 é igual, logo poderemos comentar a linha
+	de codigo referente ao CKCON e obtemos um baud rate de 9600*/
+	TMOD |= 0x02; /*Modo 2 (8 bit com auto reload) obrigatorio no uso da UART0*/
+	TR1 = 1; /*Ativiação do timer 1*/
 }
 
 void portaSerieConfigs()
 {
-    TI0 = 1;    /*Tramiste interrupt flag - não sera usada neste exemplo*/
-    RI0 = 0;    /*Receive interrupt flag*/
-    REN0 = 1;   /*Ativa a receção*/
+	TI0 = 1; /*Transmite interrupt flag*/
+	RI0 = 1; /*Recive interrupt flag*/
+	REN0 = 1; /*Ativa a receção (por defeito desativada)*/
 }
 
-unsigned char recebeUART() 
+void receiveUART()
 {
-	unsigned char x;
-	while (!RI0);   /*Aguarda a receção do byte*/ 
-	x = SBUF0;      /*Recebe o byte e le de SBBUF*/
-	RI0=0;          /*limpa a flag de rececao*/ 
-	return x;       /*Devolve o byte lido à main*/
+	while(!RI0);/*Aguarda a receção de um byte*/
+	letter = SBUF0; /*Le o byte recebido no fim da comunicação guardado temporariamente no buffer da UART0*/
+	RI0 = 0; /*Limpa a flag de receção para uma nova receção*/ 
+}
+
+void sendUART(signed char x)
+{
+	/*Visto apenas enviarmos valores entre 0 e 15 um byte é mais do que suficiente para envio dos nossos dados*/
+	while(!TI0); /*Aguarada pelo envio anterior*/
+	TI0 = 0; /*Limpa a flag de envio*/
+	SBUF0 = x; /*Envio do valor de index*/
 }
